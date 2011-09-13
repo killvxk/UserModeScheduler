398a54bee30d46da99a80ec207ac1e504bef90b6
John Davis <jdavis@pcprogramming.com>
2011-09-13 07:03:51 -0500

    saving work, no compile

--- UserModeScheduler/UserModeScheduler.h
+++ UserModeScheduler/UserModeScheduler.h
@@ -124,15 +124,45 @@ namespace UmsScheduler {
 	};
 	typedef std::shared_ptr<TUmsThreadContext> TUmsThreadContextPtr;
 
-	///////////////////////////////////
+	//////////////////////////////////////
 	class TUmsThread : public IUmsThread {
 	private:
 		TUmsThread() {}
 	private:
 		TUmsCompletionListPtr completion_list;
+	private:
+		HANDLE hThread;
+	private:
+		DWORD threadId;
 	public:
-		TUmsThread(TUmsCompletionListPtr completion_list) : completion_list(completion_list) {
-			//todo
+		TUmsThreadContext threadContext;
+	private:
+		enum { KB = 1024 };
+	public:
+		TUmsThread(TUmsCompletionListPtr completion_list, DWORD stackSize = 1) : completion_list(completion_list), hThread(NULL), threadId(0) {
+			UMS_CREATE_THREAD_ATTRIBUTES umsAttributes; Clear(umsAttributes);
+			PPROC_THREAD_ATTRIBUTE_LIST pAttributeList = NULL;
+			SIZE_T sizeAttributeList = 0;
+
+			threadContext.SetThread(this);
+
+			umsAttributes.UmsVersion = UMS_VERSION;
+			umsAttributes.UmsContext = threadContext.GetTheardContext();
+			umsAttributes.UmsCompletionList = completion_list->GetCompletionList();
+
+			Check(FALSE == ::InitializeProcThreadAttributeList(NULL, 1, 0, &sizeAttributeList));
+			Check(GetLastError() == ERROR_INSUFFICIENT_BUFFER);
+
+			std::vector<char> attributeList(sizeAttributeList);
+			Check(TRUE == ::InitializeProcThreadAttributeList(pAttributeList, 1, 0, &sizeAttributeList));
+			Check(TRUE == ::UpdateProcThreadAttribute(pAttributeList, 0, PROC_THREAD_ATTRIBUTE_UMS_THREAD, &umsAttributes, sizeof(UMS_CREATE_THREAD_ATTRIBUTES), NULL, NULL));
+
+			hThread = ::CreateRemoteThreadEx(
+				GetCurrentProcess(), NULL, stackSize*KB, UMSThreadProxyMain, 
+				this, STACK_SIZE_PARAM_IS_A_RESERVATION, pAttributeList, &hThread
+				);
+
+			::DeleteProcThreadAttributeList(pAttributeList);
 		}
 	};
 
