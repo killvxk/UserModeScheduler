b0bd16a4a983e5bf2cf3d3ebc69e24c0bf4da406
John Davis <jdavis@pcprogramming.com>
2011-09-13 05:45:16 -0500

    ums context information

--- UserModeScheduler/UserModeScheduler.h
+++ UserModeScheduler/UserModeScheduler.h
@@ -8,6 +8,19 @@
 #include <sstream>
 
 namespace UmsScheduler {
+	///////////////
+	class IThread {
+	public:
+		virtual void Run() = 0;
+	public:
+		~IThread() {}
+	};
+
+	///////////////////////////////////////
+	template <typename T> void Clear(T &t) {
+		memset(&t, 0, sizeof(t));
+	}
+
 	////////////////////////////////////////////////////////////////
 	inline void CheckMacro(bool check, std::string file, int line) {
 		if(true == check) {
@@ -40,7 +53,7 @@ namespace UmsScheduler {
 	};
 	typedef std::shared_ptr<TUmsCompletionList> TUmsCompletionListPtr;
 
-
+	/////////////////////////
 	class TUmsThreadContext {
 	private:
 		PUMS_CONTEXT ums_context;
@@ -50,6 +63,58 @@ namespace UmsScheduler {
 		}
 	public:
 		PUMS_CONTEXT GetTheardContext() { return ums_context; }
+	private:
+		BOOL SetThreadInformation(
+			UMS_THREAD_INFO_CLASS threadInfoClass, PVOID threadInfo, ULONG threadInfoLength
+			) {
+			return ::SetUmsThreadInformation(
+				ums_context, threadInfoClass, threadInfo, threadInfoLength
+				);
+		}
+	private:
+		BOOL QueryThreadInformation(
+			UMS_THREAD_INFO_CLASS threadInfoClass, PVOID threadInfo, ULONG threadInfoLength, PULONG returnLength
+			) {
+			return ::QueryUmsThreadInformation(
+				ums_context, threadInfoClass, threadInfo, threadInfoLength,	returnLength
+				);
+		}
+	public:
+		void SetThread(IThread *iThread) {
+			Check(TRUE == SetThreadInformation(
+				UmsThreadUserContext, &iThread,	sizeof(iThread)
+				));
+		}
+	public:
+		IThread *GetThread() {
+			IThread *iThread = NULL;
+			ULONG returnLength = 0;
+			Check(TRUE == QueryThreadInformation(
+				UmsThreadUserContext, &iThread, sizeof(iThread), &returnLength
+				));
+			Check(sizeof(iThread) == returnLength);
+			return iThread;
+		}
+	public:
+		BOOL IsSuspended() {
+			BOOLEAN suspended = false;
+			ULONG returnLength = 0;
+			Check(TRUE == QueryThreadInformation(
+				UmsThreadIsSuspended, &suspended, sizeof(suspended), &returnLength
+				));
+			Check(sizeof(suspended) == returnLength);
+			return (TRUE == suspended);
+		}
+	public:
+		BOOL IsTerminated() {
+			BOOLEAN terminated = false;
+			ULONG returnLength = 0;
+			Check(TRUE == QueryThreadInformation(
+				UmsThreadIsTerminated, &terminated, sizeof(terminated), &returnLength
+				));
+			Check(sizeof(terminated) == returnLength);
+			return (TRUE == terminated);
+		}
 	public:
 		virtual ~TUmsThreadContext() {
 			Check(TRUE == ::DeleteUmsThreadContext(ums_context));
