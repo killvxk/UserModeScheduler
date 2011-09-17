3cef611db82d9177d901bb4f21d52e4de2bce58d
John Davis <jdavis@pcprogramming.com>
2011-09-17 18:05:09 -0500

    test code

--- UserModeScheduler/UserModeScheduler.cpp
+++ UserModeScheduler/UserModeScheduler.cpp
@@ -4,10 +4,18 @@
 #include "stdafx.h"
 #include "UserModeScheduler.h"
 
+class TimedPrinter : public UmsScheduler::IRun {
+	DWORD Run() {
+		for(int i = 0; i < 10; i++)
+			std::cout << __FUNCTION__ << ": " << i << std::endl;
+		return 0;
+	}
+};
+
 int _tmain(int argc, _TCHAR* argv[])
 {
-	UmsScheduler::TUmsScheduler scheduler;
-	scheduler.Run();
+	UmsScheduler::TUmsScheduler::iUmsScheduler = new UmsScheduler::TUmsScheduler();
+	UmsScheduler::TUmsScheduler::iUmsScheduler->QueueWorker(std::shared_ptr<UmsScheduler::IRun>(new TimedPrinter()), UmsScheduler::Normal);
+	UmsScheduler::TUmsScheduler::iUmsScheduler->Run();
 	return 0;
 }
-
